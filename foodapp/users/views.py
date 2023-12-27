ef45379cebd365e94eec30f0d3d7e1c4a8a76c25
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2023-12-27 21:05:19 +0600

    .

--- foodapp/users/views.py
+++ foodapp/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import Register_Form
+from django.contrib.auth import authenticate
 
 
 # Create your views here.
@@ -14,8 +15,8 @@ def user_reg(request):
         if form.is_valid():
             form.save() #to save the form
             username = form.cleaned_data.get('username') #getting the username
-            messages.success(request,f'Welcome {username}! your account is created successfully')
-            return redirect('food:index')
+            messages.success(request,f'Welcome {username}! you are logged in')
+            return redirect('login')
     
     else:    
         form = Register_Form()
