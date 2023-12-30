a1968c68d0af82e4d628460c952bef34186510f4
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2023-12-30 20:06:48 +0600

    added seperate user to post

--- foodapp/food/models.py
+++ foodapp/food/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-
-
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 class Item(models.Model):
+
+    def __str__(self):
+        return self.item_name
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default= 1)
     item_name = models.CharField(max_length = 200)
     item_desc = models.CharField(max_length = 1000)
     item_price = models.IntegerField()
     item_image = models.CharField(max_length = 1000, default= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtZekdRzYGc99Ck2T6eX5xhHbWRT4IEzNfAHzaQq9ms5dqvczFUIZJJ5gqjx8joBRFK_8&usqp=CAU')
+
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
+    
--- foodapp/food/views.py
+++ foodapp/food/views.py
@@ -7,6 +7,8 @@ from .forms import ItemForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin #login req for class based
 
 from django.contrib.auth.decorators import login_required #needs login to view page
@@ -31,15 +33,20 @@ class IndexClassView(LoginRequiredMixin ,ListView):
 def item(request):
     return HttpResponse('this is item view')
 
-@login_required(login_url='login')
+#@login_required(login_url='login')
 def detail(request, item_id):
     item = Item.objects.get(pk= item_id)
     context = {
         'item': item
     }
     return render(request, 'food/detail.html',context)
+# class based for details
+class DetailView(LoginRequiredMixin, DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+    context_object_name = 'item'
 
-@login_required(login_url='login')
+#@login_required(login_url='login')
 def create_item(request):
     form = ItemForm(request.POST or None)
     context ={
@@ -50,6 +57,20 @@ def create_item(request):
         form.save()
         return redirect('food:index')
     return render(request, 'food/item-form.html', context)
+
+#class based fro create item
+class CreateItem(LoginRequiredMixin,CreateView):
+    model = Item
+    fields =['item_name','item_desc','item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
+    
+
+
+
 
 @login_required(login_url='login')
 def update_item(request , id):
